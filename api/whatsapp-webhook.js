a05edd68749f348a73cd033a60503e3834c4356b
Rosangela <96451367+Rommendes@users.noreply.github.com>
2025-05-12 14:54:52 -0300

    Vercel

---
+++ api/whatsapp-webhook.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const mensagem = req.body.Body;
+    const remetente = req.body.From;
+    
+    console.log('Mensagem recebida:', mensagem);
+    console.log('Remetente:', remetente);
+
+    // Responde a mensagem
+    res.status(200).send('<Response><Message>Recebemos sua mensagem!</Message></Response>');
+  } else {
+    res.status(405).send('Método não permitido');
+  }
+}
